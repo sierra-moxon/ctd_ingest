2c6ff70ccc710c4ad09de62a5f8b349b6e596625
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-03-07 17:24:25 -0800

    add chemical gene

---
+++ chemical_to_gene.py
@@ -0,0 +1,42 @@
+import uuid
+
+from koza.cli_runner import koza_app
+
+source_name = "ctd_chemical_to_disease"
+
+row = koza_app.get_row(source_name)
+
+if row['DirectEvidence'] in ['marker/mechanism', 'therapeutic']:
+
+    chemical = {
+        "id": 'MESH:' + row['ChemicalID'],
+        "name": row['ChemicalName'],
+        "source": 'infores:ctd'
+    }
+
+    disease = {"id": row['DiseaseID'],
+               "name": row['DiseaseID'],
+               "source": "infores:ctd"}
+
+    if row['DirectEvidence'] == 'marker/mechanism':
+        predicate = "biolink:biomarker_for"
+        relation = "RO:0002607"
+    elif row['DirectEvidence'] == 'therapeutic':
+        predicate = "treats"
+        relation = "RO:0002606"
+
+    association = {
+        "id": "uuid:" + str(uuid.uuid1()),
+        "subject": chemical.get("id"),
+        "predicate": predicate,
+        "object": disease.get("id"),
+        "relation": relation,
+        "publications": ["PMID:" + p for p in row['PubMedIDs'].split("|")],
+        "source": "infores:ctd"
+    }
+    print(chemical)
+    print(disease)
+    print(association)
+    koza_app.writer.write_node(chemical)
+    koza_app.writer.write_node(disease)
+    koza_app.writer.write_edge(association)
