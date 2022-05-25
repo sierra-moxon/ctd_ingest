31ad69891099474972c0fc14cb61f6b33d97e895
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-05-25 16:16:44 -0700

    refine click/typer

--- ctd_ingest/main.py
+++ ctd_ingest/main.py
@@ -1,25 +1,8 @@
 import typer
-from ctd_ingest import cli_utils
+from cat_merge.merge import merge
 
 typer_app = typer.Typer()
 OUTPUT_DIR = "output"
-
-
-@typer_app.command()
-def transform(
-        output_dir: str = typer.Option(OUTPUT_DIR, help="Directory to output data"),
-        do_merge: bool = typer.Option(False, "--merge", help="Merge output dir after ingest"),
-        ingest: str = typer.Option(False, help="path to ingest config yaml file"),
-):
-    """
-    Something descriptive
-    """
-    cli_utils.transform_all(
-        output_dir=output_dir,
-        ingest=ingest
-    )
-    if do_merge:
-        merge(f"{output_dir}/transform_output", output_dir)
 
 
 @typer_app.command()
@@ -33,7 +16,19 @@ def merge(
     """
     Something descriptive
     """
-    cli_utils.merge_files(input_dir=input_dir, output_dir=output_dir)
+    merge_files(input_dir=input_dir, output_dir=output_dir)
+
+
+def merge_files(
+    name: str = "ctd_pre_neo",
+    input_dir: str = f"{OUTPUT_DIR}/transform_output",
+    output_dir: str = OUTPUT_DIR,
+):
+    merge(
+        name=name,
+        input_dir=input_dir,
+        output_dir=output_dir
+    )
 
 
 if __name__ == "__main__":
