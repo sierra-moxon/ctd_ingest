83145b420d8dd8ed77b04cdf41768d8e2db16da7
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-05-25 08:38:12 -0700

    only look at decrease expression

--- ingests/ctd_chemical_to_gene/ctd_chemical_to_gene.py
+++ ingests/ctd_chemical_to_gene/ctd_chemical_to_gene.py
@@ -20,21 +20,21 @@ if row['InteractionActions'] == 'decreases^expression':
             "in_taxon": row['OrganismID'],
             "source": "infores:ctd"}
 
-    if row['InteractionActions'] == 'decreases^expression':
-        predicate = "biolink:affects"
-        object_aspect = "expression"
-        object_direction = "decrease"
+    predicate = "biolink:affects"
+    object_aspect = "expression"
+    object_direction = "decrease"
 
     association = {
         "id": "uuid:" + str(uuid.uuid1()),
         "subject": chemical.get("id"),
         "predicate": predicate,
         "object": "NCBIGene:"+gene.get("id"),
+        "object_aspect": object_aspect,
+        "object_direction": object_direction,
         "publications": ["PMID:" + p for p in row['PubMedIDs'].split("|")],
         "source": "infores:ctd"
     }
-    print(chemical)
-    print(gene)
+    print(row)
     print(association)
     koza_app.writer.write_node(chemical)
     koza_app.writer.write_node(gene)
