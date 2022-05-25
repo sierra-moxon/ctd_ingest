fddf8471fc380b86e42d4dab00cadcebd817f3c5
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-05-25 09:56:32 -0700

    add merge and cli utils formatted after monarch-ingest

---
+++ ctd_ingest/main.py
@@ -0,0 +1,37 @@
+import typer
+from ctd_ingest import cli_utils
+
+typer_app = typer.Typer()
+OUTPUT_DIR = "output"
+
+
+@typer_app.command()
+def transform(
+        output_dir: str = typer.Option(OUTPUT_DIR, help="Directory to output data"),
+        do_merge: bool = typer.Option(False, "--merge", help="Merge output dir after ingest"),
+        ingest: str = typer.Option(False, help="path to ingest config yaml file"),
+):
+    """
+    Something descriptive
+    """
+    cli_utils.transform_all(
+        output_dir=output_dir,
+        ingest=ingest
+    )
+    if do_merge:
+        merge(f"{output_dir}/transform_output", output_dir)
+
+
+@typer_app.command()
+def merge(
+        input_dir: str = typer.Option(
+            f"{OUTPUT_DIR}/transform_output",
+            help="Directory containing nodes and edges to be merged",
+        ),
+        output_dir: str = typer.Option(f"{OUTPUT_DIR}", help="Directory to output data"),
+):
+    """
+    Something descriptive
+    """
+    cli_utils.merge_files(input_dir=input_dir, output_dir=output_dir)
+
