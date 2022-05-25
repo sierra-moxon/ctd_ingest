011add8c64198f31e7b810375a47f256edd68b7a
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-05-25 09:39:30 -0700

    pull out susceptibility and cotreatement

--- ingests/ctd_chemical_to_gene/ctd_chemical_to_gene.py
+++ ingests/ctd_chemical_to_gene/ctd_chemical_to_gene.py
@@ -7,7 +7,15 @@ source_name = "ctd_chemical_to_gene"
 row = koza_app.get_row(source_name)
 
 
-if row['InteractionActions'] == 'decreases^expression':
+interaction_actions = [a for a in row['InteractionActions'].split("|")]
+# ['decreases^expression','increases^abundance']
+
+for action in interaction_actions:
+    qualified_action = action.split("^")
+    if qualified_action[1] == 'cotreatment' or qualified_action[1] == 'response to substance':
+        pass
+    aspect = qualified_action[1]
+    direction = qualified_action[0]
 
     chemical = {
         "id": 'MESH:' + row['ChemicalID'],
@@ -15,27 +23,36 @@ if row['InteractionActions'] == 'decreases^expression':
         "source": 'infores:ctd'
     }
 
-    gene = {"id": 'NCBIGene:' + row['GeneID'],
+    gene = {"id": row['GeneID'],
             "name": row['GeneSymbol'],
             "in_taxon": row['OrganismID'],
             "source": "infores:ctd"}
 
     predicate = "biolink:affects"
-    object_aspect = "expression"
-    object_direction = "decrease"
+    object_aspect = aspect
+    object_direction = direction
 
-    association = {
-        "id": "uuid:" + str(uuid.uuid1()),
-        "subject": chemical.get("id"),
-        "predicate": predicate,
-        "object": "NCBIGene:"+gene.get("id"),
-        "object_aspect": object_aspect,
-        "object_direction": object_direction,
-        "publications": ["PMID:" + p for p in row['PubMedIDs'].split("|")],
-        "source": "infores:ctd"
-    }
-    print(row)
-    print(association)
+    if object_direction == 'affects':
+        association = {
+            "id": "uuid:" + str(uuid.uuid1()),
+            "subject": chemical.get("id"),
+            "predicate": predicate,
+            "object": gene.get("id"),
+            "object_aspect": object_aspect,
+            "publications": ["PMID:" + p for p in row['PubMedIDs'].split("|")],
+            "source": "infores:ctd"
+        }
+    else:
+        association = {
+            "id": "uuid:" + str(uuid.uuid1()),
+            "subject": chemical.get("id"),
+            "predicate": predicate,
+            "object": gene.get("id"),
+            "object_aspect": object_aspect,
+            "object_direction": object_direction,
+            "publications": ["PMID:" + p for p in row['PubMedIDs'].split("|")],
+            "source": "infores:ctd"
+        }
     koza_app.writer.write_node(chemical)
     koza_app.writer.write_node(gene)
     koza_app.writer.write_edge(association)
