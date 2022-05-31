8689b955fcf38d18f467be907bba3fa3de6cb9ce
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2022-05-31 15:38:47 -0700

    refine ctd ingest

--- ingests/ctd_chemical_to_gene/ctd_chemical_to_gene.py
+++ ingests/ctd_chemical_to_gene/ctd_chemical_to_gene.py
@@ -10,51 +10,58 @@ row = koza_app.get_row(source_name)
 interaction_actions = [a for a in row['InteractionActions'].split("|")]
 # ['decreases^expression','increases^abundance']
 
-for action in interaction_actions:
-    qualified_action = action.split("^")
-    if qualified_action[1] == 'cotreatment' or qualified_action[1] == 'response to substance':
-        continue
-    aspect = qualified_action[1]
-    direction = qualified_action[0]
+if len(interaction_actions) > 1:
+    pass
+else:
+    for action in interaction_actions:
+        qualified_action = action.split("^")
+        if qualified_action[1] == 'cotreatment' or qualified_action[1] == 'response to substance':
+            continue
+        aspect = qualified_action[1]
+        if qualified_action[0] == "increases":
+            qualified_action = "increased"
+        if qualified_action[0] == "decreases":
+            qualified_action = "decreased"
+        direction = qualified_action
 
-    chemical = {
-        "id": 'MESH:' + row['ChemicalID'],
-        "name": row['ChemicalName'],
-        "source": 'infores:ctd',
-        "category": "biolink:ChemicalEntity"
-    }
-
-    gene = {"id": "NCBIGene:" + row['GeneID'],
-            "name": row['GeneSymbol'],
-            "in_taxon": row['OrganismID'],
-            "source": "infores:ctd",
-            "category": "biolink:Gene"}
-
-    predicate = "biolink:affects"
-    object_aspect = aspect
-    object_direction = direction
-
-    if object_direction == 'affects':
-        association = {
-            "id": "uuid:" + str(uuid.uuid1()),
-            "subject": chemical.get("id"),
-            "predicate": predicate,
-            "object": gene.get("id"),
-            "object_aspect": object_aspect,
-            "publications": ["PMID:" + p for p in row['PubMedIDs'].split("|")],
-            "source": "infores:ctd"
+        chemical = {
+            "id": 'MESH:' + row['ChemicalID'],
+            "name": row['ChemicalName'],
+            "source": 'infores:ctd',
+            "category": "biolink:ChemicalEntity"
         }
-    else:
-        association = {
-            "id": "uuid:" + str(uuid.uuid1()),
-            "subject": chemical.get("id"),
-            "predicate": predicate,
-            "object": gene.get("id"),
-            "object_aspect": object_aspect,
-            "object_direction": object_direction,
-            "publications": ["PMID:" + p for p in row['PubMedIDs'].split("|")],
-            "source": "infores:ctd"
-        }
-    koza_app.writer.write_node(chemical)
-    koza_app.writer.write_node(gene)
-    koza_app.writer.write_edge(association)
+
+        gene = {"id": "NCBIGene:" + row['GeneID'],
+                "name": row['GeneSymbol'],
+                "in_taxon": row['OrganismID'],
+                "source": "infores:ctd",
+            "   category": "biolink:Gene"}
+
+        predicate = "biolink:affects"
+        object_aspect = aspect
+        object_direction = direction
+
+        if object_direction == 'affects':
+            association = {
+                "id": "uuid:" + str(uuid.uuid1()),
+                "subject": chemical.get("id"),
+                "predicate": predicate,
+                "object": gene.get("id"),
+                "object_aspect": object_aspect,
+                "publications": ["PMID:" + p for p in row['PubMedIDs'].split("|")],
+                "source": "infores:ctd"
+            }
+        else:
+            association = {
+                "id": "uuid:" + str(uuid.uuid1()),
+                "subject": chemical.get("id"),
+                "predicate": predicate,
+                "object": gene.get("id"),
+                "object_aspect": object_aspect,
+                "object_direction": object_direction,
+                "publications": ["PMID:" + p for p in row['PubMedIDs'].split("|")],
+                "source": "infores:ctd"
+            }
+        koza_app.writer.write_node(chemical)
+        koza_app.writer.write_node(gene)
+        koza_app.writer.write_edge(association)
